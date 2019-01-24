b3396463916c3022c94817a1f1360a50b26b6862
Artenes Nogueira <artenesama@gmail.com>
2019-01-23 21:25:32 -0400

    Fixes queries for top albums, all were using the same table

--- app/src/main/java/io/github/artenes/ostatic/db/DatabaseDaos.kt
+++ app/src/main/java/io/github/artenes/ostatic/db/DatabaseDaos.kt
@@ -12,10 +12,10 @@ interface AlbumDao {
     @Query("select position, id, name, files, size, added, time, (select url from covers where covers.album_id = albums.id limit 1) as cover from top_100_all_time inner join albums on id = album_id order by position ASC limit :limit")
     suspend fun getTop100AllTime(limit: Int = 100): List<TopAlbumView>
 
-    @Query("select position, id, name, files, size, added, time, (select url from covers where covers.album_id = albums.id limit 1) as cover from top_100_all_time inner join albums on id = album_id order by position ASC limit :limit")
+    @Query("select position, id, name, files, size, added, time, (select url from covers where covers.album_id = albums.id limit 1) as cover from top_100_last_6_months inner join albums on id = album_id order by position ASC limit :limit")
     suspend fun getTop100Last6Months(limit: Int = 100): List<TopAlbumView>
 
-    @Query("select position, id, name, files, size, added, time, (select url from covers where covers.album_id = albums.id limit 1) as cover from top_100_all_time inner join albums on id = album_id order by position ASC limit :limit")
+    @Query("select position, id, name, files, size, added, time, (select url from covers where covers.album_id = albums.id limit 1) as cover from top_100_newly_added inner join albums on id = album_id order by position ASC limit :limit")
     suspend fun getTop100NewlyAdded(limit: Int = 100): List<TopAlbumView>
 
 }
