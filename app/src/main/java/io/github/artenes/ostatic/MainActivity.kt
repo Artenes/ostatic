d1ba66a95a7d41bbf714efef0b35cd09cb315a0f
Artenes Nogueira <artenesama@gmail.com>
2019-01-27 16:45:11 -0400

    Adds bottom navigation in the MainActivity

--- app/src/main/java/io/github/artenes/ostatic/MainActivity.kt
+++ app/src/main/java/io/github/artenes/ostatic/MainActivity.kt
@@ -1,15 +1,47 @@
 package io.github.artenes.ostatic
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import io.github.artenes.ostatic.view.HomeFragment
+import io.github.artenes.ostatic.view.LibraryFragment
+import io.github.artenes.ostatic.view.SearchFragment
+import kotlinx.android.synthetic.main.main_activity.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_activity)
-        supportFragmentManager.beginTransaction().replace(R.id.content, HomeFragment()).commit()
+        setContentView(R.layout.main_activity)
+        bottomNavigationView.setOnNavigationItemSelectedListener(this)
+        changeToHomeTab()
+    }
+
+    fun changeToHomeTab() {
+        replaceFragment(HomeFragment())
+    }
+
+    fun changeToSearchTab() {
+        replaceFragment(SearchFragment())
+    }
+
+    fun changeToLibraryTab() {
+        replaceFragment(LibraryFragment())
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_home -> changeToHomeTab()
+            R.id.action_search -> changeToSearchTab()
+            R.id.action_library -> changeToLibraryTab()
+        }
+        return true
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.content, fragment).commit()
     }
 
 }
