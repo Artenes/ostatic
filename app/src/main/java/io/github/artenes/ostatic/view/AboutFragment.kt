4fc9f09f61b269d1042ab155fa5cc8e6c81ba9d1
Artenes Nogueira <artenesama@gmail.com>
2019-01-27 21:44:06 -0400

    Fixes white flash when opening about tab

--- app/src/main/java/io/github/artenes/ostatic/view/AboutFragment.kt
+++ app/src/main/java/io/github/artenes/ostatic/view/AboutFragment.kt
@@ -1,5 +1,6 @@
 package io.github.artenes.ostatic.view
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,6 +12,7 @@ class AboutFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val webview = WebView(container?.context)
+        webview.setBackgroundColor(Color.TRANSPARENT)
         webview.loadUrl("file:///android_asset/about.html")
         return webview
     }
